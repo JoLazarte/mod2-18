167f9cd26186007f562c9c7738eac6c55a18cb58
Joanna <jlazartelagos@gmail.com>
2021-04-07 00:36:01 -0300

    completado

--- color-aleatorio/index.js
+++ color-aleatorio/index.js
@@ -8,3 +8,28 @@ de r, g y b se generan aleatoriamente
 (con números del 1 al 255).
 
 */
+
+const body = document.querySelector("body");
+
+const obtenerColorAleatorio = (event) => {
+    if (event.code == "Space") {
+        let stringColor = "rgb";
+        const colorRGB = stringColor.split("");
+        console.log(colorRGB);
+        let r = Number(colorRGB[0]) 
+        r = rand();
+        let g = Number(colorRGB[1]) 
+        g = rand();
+        let b = Number(colorRGB[2]) 
+        b = rand();
+        body.style = `background-color: rgb(${r},${g},${b});`;
+       
+    }
+};
+
+const rand = () => {
+    let randomNum = Math.random() * (255 - 0) + 0;
+    return Math.round(randomNum);
+};
+
+body.addEventListener('keyup', obtenerColorAleatorio);
